b9f5781454f42cca7fa568d903a3b24fc642eda4
bhuimrna <imre.nagy@flex.com>
2019-07-17 15:35:28 +0200

    added migration publishing

--- src/AdditionalDetailsServiceProvider.php
+++ src/AdditionalDetailsServiceProvider.php
@@ -13,6 +13,29 @@ class AdditionalDetailsServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        $this->registerPublishing();
+    }
+
+    /**
+     * Register the package's publishable resources.
+     *
+     * @return void
+     */
+    protected function registerPublishing()
+    {
+        if ($this->app->runningInConsole()) {
+            $this->publishes([
+                __DIR__.'/../config/additional_details.php' => base_path('config/additional_details.php'),
+            ], 'config');
+
+            $this->publishes([
+                __DIR__.'/../database/migrations/create_additional_detail_definitions_table.stub' => database_path(
+                    sprintf('migrations/%s_create_additional_detail_definitions_table.php', date('Y_m_d_His'))
+                ),
+                __DIR__.'/../database/migrations/create_additional_details_table.stub' => database_path(
+                    sprintf('migrations/%s_create_additional_details_table.php', date('Y_m_d_His'))
+                )
+            ], 'migrations');
+        }
     }
 }

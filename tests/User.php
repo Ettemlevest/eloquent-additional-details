88b9aa3a993717252188dbe3eaa8e426767c0e2f
Imre Nagy <ettemlevest@outlook.com>
2019-07-25 10:20:54 +0200

    added HasDetails trait to User

--- tests/User.php
+++ tests/User.php
@@ -7,10 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Auth\Access\Authorizable;
 use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
+use Ettemlevest\AdditionalDetails\HasDetails;
 
 class User extends Model implements AuthorizableContract, AuthenticatableContract
 {
-    use Authorizable, Authenticatable;
+    use HasDetails, Authorizable, Authenticatable;
 
     /**
      * The attributes that are mass assignable.

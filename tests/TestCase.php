ea15ed9eff04e783e722634c502a71b88e577903
Imre Nagy <ettemlevest@outlook.com>
2019-07-25 09:53:28 +0200

    added env and db setup to basic TestCase

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,12 +4,15 @@ namespace Ettemlevest\AdditionalDetails\Tests;
 
 use Orchestra\Testbench\TestCase as Orchestra;
 use Ettemlevest\AdditionalDetails\AdditionalDetailsServiceProvider;
+use Illuminate\Database\Schema\Blueprint;
 
-class TestCase extends Orchestra
+abstract class TestCase extends Orchestra
 {
     public function setUp(): void
     {
         parent::setUp();
+
+        $this->setUpDatabase($this->app);
     }
 
     protected function getPackageProviders($app)
@@ -17,5 +20,41 @@ class TestCase extends Orchestra
         return [
             AdditionalDetailsServiceProvider::class,
         ];
+    }
+
+    /**
+     * Set up the environment
+     *
+     * @param \Illuminate\Foundation\Application $app
+     * @return void
+     */
+    protected function getEnvironmentSetUp($app)
+    {
+        $app['config']->set('database.default', 'sqlite');
+        $app['config']->set('database.connections.sqlite', [
+            'driver' => 'sqlite',
+            'database' => ':memory:',
+            'prefix' => '',
+        ]);
+    }
+
+    /**
+     * Set up the database.
+     *
+     * @param \Illuminate\Foundation\Application $app
+     * @return void
+     */
+    protected function setUpDatabase($app)
+    {
+        $app['db']->connection()->getSchemaBuilder()->create('users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('email');
+        });
+
+        include_once __DIR__.'/../database/migrations/create_additional_detail_definitions_table.php.stub';
+        include_once __DIR__.'/../database/migrations/create_additional_details_table.php.stub';
+
+        (new \CreateAdditionalDetailDefinitionsTable)->up();
+        (new \CreateAdditionalDetailsTable)->up();
     }
 }
